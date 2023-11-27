d3b77c45e6ad372c0cd4f81088441d889ab2eab7
polineniavinash <polineniavinash222@gmail.com>
2023-11-26 18:32:55 -0600

    Add working code

---
+++ README.md
@@ -0,0 +1,41 @@
+---
+title: SimilarWordFinderLevel100
+emoji: 👁
+colorFrom: gray
+colorTo: purple
+sdk: streamlit
+sdk_version: 1.28.2
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+# GenAI-SimilarWordFinder
+
+## Overview
+GenAI-SimilarWordFinder is a Streamlit-powered web application designed to showcase the capabilities of generative AI in providing insights and solutions for various enterprise-related queries, with a focus on fields like Supply Chain Optimization, Customer Service Enhancement, Market Analysis, and Financial Forecasting.
+
+## Features
+- Interactive UI to input queries and select enterprise domains.
+- AI-driven insights tailored for different professional profiles (Intern, Senior Engineer, ML Expert, Product Manager).
+- Customizable word limit for responses.
+- Section for additional resources and contact information.
+
+## Setup
+1. Clone the repository.
+2. Install required packages: `pip install -r requirements.txt`.
+3. Set your OpenAI API key in your environment: `export OPENAI_API_KEY="Your-Open-AI-API-Key"`.
+
+## Usage
+- Launch the app with `streamlit run app.py`.
+- Choose a professional profile and an enterprise domain.
+- Enter your query and click "Generate Insights".
+
+## Contributing
+Feel free to fork this project and adapt it to your needs. Contributions for improving the application are welcome.
+
+## Acknowledgements
+Created by Avinash Polineni, leveraging cutting-edge Generative AI technologies to drive enterprise innovation.
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import os
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Set page configuration
+st.set_page_config(page_title="Enterprise AI Solutions by Avinash Polineni", page_icon=":briefcase:")
+st.title("Enterprise AI Solutions by Avinash Polineni")
+
+# Initialize OpenAIEmbeddings
+embeddings = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Load data from a more focused CSV file
+from langchain.document_loaders.csv_loader import CSVLoader
+loader = CSVLoader(file_path='enterpriseData.csv', csv_args={
+    'delimiter': ',',
+    'quotechar': '"',
+    'fieldnames': ['Keywords', 'Category']
+})
+
+# Load and process data
+data = loader.load()
+db = FAISS.from_documents(data, embeddings)
+
+# UI for user input
+st.header("Explore AI-Driven Insights Across Enterprise Domains")
+st.markdown("""
+### How It Works:
+Enter a query related to enterprise topics like 'Supply Chain Optimization', 'Cybersecurity', or 'Market Analysis'. The AI will provide similar keywords and categories, offering insights and expanding your understanding of the topic.
+
+### Example Queries:
+- "Cybersecurity in Supply Chains"
+- "AI in Customer Relationship Management"
+- "Data Analytics in Market Forecasting"
+
+**Now, try entering your own query!**
+""")
+
+user_input = st.text_input("Your Query: ", key='user_input')
+submit = st.button('Generate Insights')
+
+if submit:
+    # Find similar entries
+    similar_entries = db.similarity_search(user_input)
+    if similar_entries:
+        st.subheader("AI Generated Insights")
+        for entry in similar_entries[:3]:  # Show top 3 matches
+            st.write(entry.page_content)
+    else:
+        st.write("No relevant matches found.")
+
+# Feedback mechanism
+st.write("Was this information helpful?")
+feedback = st.radio("", ('Yes', 'No'), key='feedback')
+if feedback:
+    st.write("Thank you for your feedback!")
+
+# Footer and additional resources
+st.sidebar.markdown("### Additional Resources")
+st.sidebar.markdown("[Checkout Avinash's Hugging Face Profile](https://huggingface.co/AvinashPolineni)")
+st.sidebar.markdown("[Checkout Avinash's GitHub Profile](https://github.com/polineniavinash)")
+st.sidebar.markdown("[Contact Me on LinkedIn](https://linkedin.com/in/avinash-polineni/)")
+st.markdown("---")
+st.caption("© 2023 Avinash Polineni. All Rights Reserved.")
+
+# Reminder: Update 'enterpriseData.csv' with more relevant entries specific to blockchain and supply chain optimization.
